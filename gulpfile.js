1a4fdc8cdbaabd15812fdab897a247a06afb2374
Nabeel Molham <nmolham@godaddy.com>
2022-09-12 14:53:59 +1000

    Tide up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,73 +9,59 @@ const pump        = require( 'pump' );
 const wp_pot      = require( 'gulp-wp-pot' );
 
 const update_assets_version = function () {
-
-	file_system.writeFile( './assets/last_update', (new Date()).toISOString().replace( /[^0-9]/g, '' ), function ( error ) {
-
+	file_system.writeFile( './assets/last_update', ( new Date() ).toISOString().replace( /[^0-9]/g, '' ), function ( error ) {
 		if ( error ) {
-
 			return console.log( error );
-
 		}
 
 		console.log( "The file was saved!" );
-
 	} );
-
 };
 
 gulp.task( 'make_pot', function ( done ) {
-
 	gulp.src( './**/*.php' )
 	.pipe( wp_pot( {
-		domain        : 'wp-plugin-domain',
-		package       : 'WP Plugins Boilerplate',
-		headers       : {
-			poedit                 : true,
-			'x-poedit-keywordslist': true
+		domain         : 'wp-plugin-domain',
+		package        : 'WP Plugins Boilerplate',
+		headers        : {
+			poedit                  : true,
+			'x-poedit-keywordslist' : true
 		},
-		lastTranslator: 'Nabeel Molham <n.molham@gmail.com>'
+		lastTranslator : 'Nabeel Molham <n.molham@gmail.com>'
 	} ) )
 	.pipe( gulp.dest( './languages/template.pot' ) );
 
 	done();
-
 } );
 
 gulp.task( 'styles', function ( done ) {
-
-	gulp.src( ['./assets/src/css/**/*.scss', './assets/src/css/**/*.css'] )
+	gulp.src( [ './assets/src/css/**/*.scss', './assets/src/css/**/*.css' ] )
 	.pipe( sourcemaps.init() )
 	.pipe( sass().on( 'error', sass.logError ) )
-	.pipe( cleanCSS( { compatibility: 'ie8' } ) )
+	.pipe( cleanCSS( { compatibility : 'ie8' } ) )
 	.pipe( sourcemaps.write( './maps' ) )
 	.pipe( gulp.dest( './assets/dist/css/' ) )
 	.pipe( browserSync.stream() );
 
 	update_assets_version();
-
 	done();
-
 } );
 
 gulp.task( 'compress_js', function ( done ) {
-
 	pump( [
 		gulp.src( './assets/src/js/**/*.js' ),
 		uglify( {
-			mangle: {
-				eval: true
+			mangle : {
+				eval : true
 			}
 		} ),
 		gulp.dest( './assets/dist/js/' )
 	], done );
 
 	update_assets_version();
-
 } );
 
 const start_watch_assets = function () {
-
 	gulp.watch( [
 		'assets/src/css/**/*.scss',
 		'assets/src/css/**/*.css',
@@ -89,25 +75,18 @@ const start_watch_assets = function () {
 	gulp.watch( [
 		'**/*.php'
 	], gulp.series( 'make_pot' ) );
-
 };
 
 gulp.task( 'watch_assets', gulp.series( 'styles', 'compress_js', 'make_pot', function ( done ) {
-
 	start_watch_assets();
-
 	done();
-
 } ) );
 
 gulp.task( 'serve', gulp.series( 'styles', 'compress_js', 'make_pot', function ( done ) {
-
 	browserSync.init( {
-		proxy: "localhost"
+		proxy : "localhost"
 	} );
 
 	start_watch_assets();
-
 	done();
-
 } ) );
